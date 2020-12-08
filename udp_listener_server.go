e7f9144b86e1573025ca4edc508e0647979f910c
Adam724 <alaspina6@gmail.com>
2020-12-08 18:43:42 +0000

    formatting

--- udp_listener_server.go
+++ udp_listener_server.go
@@ -1,29 +1,27 @@
-package main
-import (
-    "fmt" 
-    "net"  
-)
+package main;
+import ("fmt");
+import ("net");
 
 
 
 func main() {
-    p := make([]byte, 2048)
+    p := make([]byte, 2048);
     addr := net.UDPAddr{
         Port: 3003,
         IP: net.ParseIP("127.0.0.1"),
-    }
-    ser, err := net.ListenUDP("udp", &addr)
+    };
+    ser, err := net.ListenUDP("udp", &addr);
     if err != nil {
-        fmt.Printf("Some error %v\n", err)
-        return
-    }
-    fmt.Println("Listening...")
+        fmt.Printf("Some error %v\n", err);
+        return;
+    };
+    fmt.Println("Listening...");
     for {
-        _,remoteaddr,err := ser.ReadFromUDP(p)
-        fmt.Printf("Read a message from %v %s \n", remoteaddr, p)
+        _,remoteaddr,err := ser.ReadFromUDP(p);
+        fmt.Printf("Read a message from %v %s \n", remoteaddr, p);
         if err !=  nil {
-            fmt.Printf("Some error  %v", err)
-            continue
-        }
-    }
-}
+            fmt.Printf("Some error  %v", err);
+            continue;
+        };
+    };
+};
